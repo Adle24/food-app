95a528f263b49f4b95f99be97bf84feb03328bcc
Adle24 <askaradilet41@gmail.com>
2025-08-27 19:30:28 +0500

    finished auth

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,78 @@
+import { CreateUserPrams, SignInParams } from "@/type";
+import {
+  Account,
+  Avatars,
+  Client,
+  ID,
+  Query,
+  TablesDB,
+} from "react-native-appwrite";
+
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
   platform: "com.adle.foodapp",
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
+};
+
+export const client = new Client();
+export const tables = new TablesDB(client);
+export const account = new Account(client);
+const avatars = new Avatars(client);
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+export const createUser = async ({
+  email,
+  password,
+  name,
+}: CreateUserPrams) => {
+  try {
+    const newAccount = await account.create(ID.unique(), email, password, name);
+    if (!newAccount) throw Error;
+
+    await signIn({ email, password });
+
+    const avatarUrl = avatars.getInitialsURL(name);
+
+    return await tables.createRow(
+      appwriteConfig.databaseId,
+      "user",
+      ID.unique(),
+      { email, name, accountId: newAccount.$id, avatar: avatarUrl }
+    );
+  } catch (e) {
+    throw new Error(e as string);
+  }
+};
+
+export const signIn = async ({ email, password }: SignInParams) => {
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+  } catch (e) {
+    throw new Error(e as string);
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const currentAccount = await account.get();
+    if (!currentAccount) throw Error;
+
+    const currentUser = await tables.listRows(
+      appwriteConfig.databaseId,
+      "user",
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.rows[0];
+  } catch (e) {
+    console.log(e);
+    throw new Error(e as string);
+  }
 };
